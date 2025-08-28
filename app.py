98bed5dfb87be51463b20923b5acdc9342655ac5
alexandredgns <alexandredgns@gmail.com>
2025-08-28 18:21:07 -0300

    Add Table ToDoLists - ForeignKey

--- app.py
+++ app.py
@@ -16,9 +16,16 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f"<Todo {self.id}, {self.name}>"
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
 
 with app.app_context():
     db.create_all()
