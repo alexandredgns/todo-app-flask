3d09e8b81f4325935308202b66ae6678f6d93693
alexandredgns <alexandredgns@gmail.com>
2025-08-29 12:39:15 -0300

    Changes on the View - ToDo's LISTS

--- app.py
+++ app.py
@@ -30,10 +30,6 @@ class TodoList(db.Model):
 with app.app_context():
     db.create_all()
 
-@app.route('/')
-def index():
-    return render_template('index.html', data=Todo.query.all())
-
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
     error = False
@@ -82,3 +78,14 @@ def delete_todo(todo_id):
     finally:
         db.session.close()
     return jsonify({ 'success': True })
+
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html',
+                lists=TodoList.query.all(),
+                active_list=TodoList.query.get(list_id),
+                todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
+@app.route('/')
+def index():
+    return redirect(url_for('get_list_todos', list_id=1))
